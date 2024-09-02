570ef73f8f7213068d5afedb664d7b3e71058adf
Bruno Peixoto <brunolnetto@gmail.com>
2024-09-02 12:07:24 -0300

    config: alembic files

---
+++ alembic/versions/bc829a1545ce_timestamp_inserted_at.py
@@ -0,0 +1,26 @@
+"""timestamp_inserted_at
+
+Revision ID: bc829a1545ce
+Revises: 
+Create Date: 2024-09-02 10:48:37.400047
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bc829a1545ce'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
