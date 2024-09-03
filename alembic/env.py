48dd1a1386d7c1405de72c7575a49ba596097a8a
Bruno Peixoto <brunolnetto@gmail.com>
2024-09-03 18:01:15 -0300

    feat: alembic

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,7 @@ from alembic import context
 
 from app.config import settings
 # Import your SQLAlchemy models
-from app.database import Base
+from app.database.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
