f8a4aa37f3933b9ce28012a0070fecc33a92374e
Bruno Peixoto <brunolnetto@gmail.com>
2024-09-06 08:24:10 -0300

    hotfix: substitute Request by task repository

--- backend/app/routers/logs.py
+++ backend/app/routers/logs.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter, Request
 from typing import Dict, Any
 
-from backend.app.repositories.logs import RequestLogsRepositoryDependency
+from backend.app.repositories.logs import (
+    RequestLogsRepositoryDependency,
+    TaskLogsRepositoryDependency,
+)
 from backend.app.rate_limiter import limiter
 from backend.app.config import settings
 
 router = APIRouter(prefix="/logs", tags=["Logs"])
 
 
+# curl -X POST "http://localhost:8000/api/logs/test_log_request" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"test\": \"data\"}"
 @router.post("/test_log_request")
 @limiter.limit(settings.DEFAULT_RATE_LIMIT)
 def create_log(
@@ -31,10 +35,10 @@ async def read_request_logs(
 
 @router.get("/tasks")
 @limiter.limit(settings.DEFAULT_RATE_LIMIT)
-async def read_tasks_logs(
-    request: Request,
-    request_log_repository: RequestLogsRepositoryDependency,
-    offset: int = 0,
-    limit: int = 100,
-):
-    return request_log_repository.get_all(offset=offset, limit=limit)
+async def read_logs(
+        request: Request,
+        task_log_repository: TaskLogsRepositoryDependency, 
+        offset: int = 0, limit: int = 100
+    ):
+    return task_log_repository.get_all(offset=offset, limit=limit)
+
